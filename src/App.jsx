9509390670e867114de76b50bf2dd7f475e3f9ab
Osiris8 <miganosiris8@gmaaail.com>
2025-07-12 17:55:52 +0200

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Home from "./components/Home";
+import Logout from "./components/Logout";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/logout" element={<Logout />} />
           </Routes>
         </Router>
       </div>
